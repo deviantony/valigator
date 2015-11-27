03c5d96844a843dc143ce6e967f5fa2edd66c700
tony <alapenna@workit.fr>
2015-11-27 11:31:29 +0100

    Removed open mode for tarfile extraction.

--- filesystemmanager.py
+++ filesystemmanager.py
@@ -5,6 +5,6 @@ class FileSystemManager:
 
     def extract_archive(self, archive_path, destination_path):
         """Extracts an archive somewhere on the filesystem."""
-        tar = open(archive_path, 'r:gz')
+        tar = open(archive_path)
         tar.errorlevel = 1
         tar.extractall(destination_path)
