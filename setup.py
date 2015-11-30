40ff558c3b8c90c33bec7fdecb7563759c068f83
tony <alapenna@workit.fr>
2015-11-30 12:12:16 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import os
+
+package_root = os.path.dirname(__file__)
+
+about = {}
+with open("xtrabackup/__about__.py") as fp:
+    exec(fp.read(), about)
+
+# Error-handling here is to allow package to be built w/o README.rst included
+try:
+    long_description = open(os.path.join(package_root, 'README.rst')).read()
+except IOError:
+    long_description = ''
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+
+    description=about["__summary__"],
+    long_description=long_description,
+
+    url=about["__uri__"],
+
+    author=about["__author__"],
+    author_email=about["__email__"],
+
+    license=about["__license__"],
+
+    classifiers=[
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Archiving :: Backup',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='database backup docker',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'sql']),
+
+    install_requires=['bottle', 'PyYAML', 'docker-py'],
+
+    entry_points={
+        'console_scripts': [
+            'valigator=valigator.valigator:main'
+        ],
+    },
+)
---
+++ valigator/__about__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__",
+    "__author__", "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "valigator"
+__summary__ = "Validate your backups !"
+__uri__ = "https://github.com/deviantony/valigator"
+
+__version__ = "0.1.0"
+
+__author__ = "Anthony Lapenna"
+__email__ = "lapenna.anthony@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2015 Anthony Lapenna"
