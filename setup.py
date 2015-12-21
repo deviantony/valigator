c3caa407ef5ce6d1c9a83b26ac700e93807b2bc8
tony <alapenna@workit.fr>
2015-12-21 10:17:55 +0100

    Python2 support.

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ setup(
 
         'License :: OSI Approved :: Apache Software License',
 
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
     ],
 
     keywords='database backup docker',
